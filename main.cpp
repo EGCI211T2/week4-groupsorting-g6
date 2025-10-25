0f63404805fe4d7c41558b7709ea4a66f59498e7
pakka-chan <pakkanat.apr@student.mahidol.edu>
2025-10-25 08:02:12 +0000

    code

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string.h>
+#include "sorting.h"
+
+int main(int argc, char *argv[]) {
+
+  cout << "argc is " << argc << endl;
+
+  char *sortType = argv[1];
+
+ int i, N;
+    int *a;
+
+    N = argc - 2;      
+    a = new int[N];
+    for(i = 0; i < N; i++){
+        *a = atoi(argv[i + 2]); 
+        a++;
+    }
+    a -= N;
+
+    cout << "Before sorting: ";
+    display(a, N);
+
+    if (strcmp(sortType, "insert") == 0) {
+        cout << "\n=== Insertion Sort ===" << endl;
+        insert_sort(a, N);
+    } 
+    else if (strcmp(sortType, "bubble") == 0) {
+        cout << "\n=== Bubble Sort ===" << endl;
+        sort(a, N);
+    } 
+    else if (strcmp(sortType, "selection") == 0) {
+        cout << "\n=== Selection Sort ===" << endl;
+        select_sort(a, N);
+    }
+
+    cout << "After sorting: ";
+    display(a, N);
+
+    delete [] a;
+    return 0;
+}
